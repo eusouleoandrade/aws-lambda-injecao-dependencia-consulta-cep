6f4094740894a4b9a942ecbf7d6bc3eaf9acf4cc
Leandro Andrade <eusouleoandrade@gmail.com>
2022-04-21 01:17:23 -0300

    Cria e configura o projeto de teste.

---
+++ ConsultaCep/test/ConsultaCep.Tests/Application/UseCases/GetAddresUseCaseTest.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace ConsultaCep.Presentation.Function.Tests.Application.UseCases
+{
+    public class GetAddresUseCaseTest
+    {
+        public GetAddresUseCaseTest()
+        {
+        }
+        
+        [Fact]
+        public async void ShouldExecuteSucessfully()
+        {
+        }
+    }
+}
